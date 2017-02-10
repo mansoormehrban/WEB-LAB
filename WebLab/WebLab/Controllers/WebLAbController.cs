81f09755a2d597d10749a6e3993de1b65d70d606
Bahria University <fall2017@bahria.edu.pk>
2017-02-10 10:05:08 +0500

    lab 1 html file uploading

---
+++ WebLab/WebLab/Controllers/WebLAbController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLab.Controllers
+{
+    public class WebLAbController : Controller
+    {
+        //
+        // GET: /WebLAb/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LAB1()
+        {
+            return View();
+        }
+	}
+}
